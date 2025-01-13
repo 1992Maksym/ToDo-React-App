5aa65301b5624e493372635980b34237a955c856
Максим <maksimsalov8@gmail.com>
2025-01-13 15:59:21 +0200

    add icons for post

--- src/components/toDoForm/ToDoForm.jsx
+++ src/components/toDoForm/ToDoForm.jsx
@@ -7,7 +7,8 @@ export default function ToDoForm({setInputValue}){
 
     function setInput(e){
         e.preventDefault()
-        setInputValue(inputItem.current.value)
+        // setInputValue(inputItem.current.value)
+        setInputValue({value: inputItem.current.value, done: false})
         setText('')
     }
 
