8acc781c3f8586efd88d695b532a25a9edbd4176
Максим <maksimsalov8@gmail.com>
2025-01-14 00:24:44 +0200

    add functional for buttons

--- src/components/toDoList/ToDoList.jsx
+++ src/components/toDoList/ToDoList.jsx
@@ -2,12 +2,12 @@ import classes from './ToDoList.module.scss'
 import ToDoItem from '../toDoItem/ToDoItem'
 import ToDoActions from '../toDoActions/ToDoActions'
 
-export default function ToDoList({posts,changeStatus,removePost}){
+export default function ToDoList({posts,changeStatus,removePost,deletePosts,deleteMarked}){
 
     return(
         <>
             {posts && <div className={classes.posts}>
-                <ToDoActions />
+                <ToDoActions deletePosts={deletePosts} deleteMarked={deleteMarked}/>
                 {posts.map((post, index) => {
                     return <ToDoItem 
                                 key={index} 
